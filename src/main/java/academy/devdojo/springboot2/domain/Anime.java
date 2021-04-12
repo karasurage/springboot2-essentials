d977b7ee2fd4305150db18f3fdeee9c71d17536b
Nicholas Mateus <nicholas.mateus@gmail.com>
2021-04-12 14:07:36 -0300

    Implementando os testes

--- src/main/java/academy/devdojo/springboot2/domain/Anime.java
+++ src/main/java/academy/devdojo/springboot2/domain/Anime.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 
 @Data
 @AllArgsConstructor
@@ -16,5 +17,6 @@ public class Anime {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotEmpty(message = "The anime name cannot be empty")
     private String name;
 }
